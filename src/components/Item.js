00601e0f9146d7b470c12306b92d4cdda5a0cebb
Min Cho <minsangcho19@gmail.com>
2022-01-13 13:21:14 -0800

    not done yet

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [status, setStatus] = useState("");
+
+  function handleClick(){
+    if(status === ""){
+      setStatus("in-cart") 
+      document.querySelector(".add").value = "Remove From Cart"
+    }
+    else{
+      setStatus("");
+      document.querySelector(".add").value = "Add to Cart"
+    }
+  }
   return (
-    <li className="">
+    <li className={status}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,27 @@
-import React from "react";
+//import React from "react";
 import Item from "./Item";
+import React, {useState} from "react"
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setCategory] = useState("All");
+  const [newItemArray, setItemArray] = useState(items);
+
+  function handleChange(e){
+    setCategory(e.target.value);
+    setItemArray(
+      if(selectedCategory === "All"){
+        return items
+      } 
+      else items.filter((item) => item.category === selectedCategory)
+      
+    console.log(selectedCategory)
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange = {handleChange} name={selectedCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +29,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {newItemArray.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
